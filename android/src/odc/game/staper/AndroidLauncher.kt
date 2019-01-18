e7242b2f73149f63c9660be0bbd2cd86491363ee
Petrov Anton <gg4every@gmail.com>
2019-01-18 22:57:41 +0400

    Initial commit
    Signed-off-by: Petrov Anton <gg4every@gmail.com>

---
+++ android/src/odc/game/staper/AndroidLauncher.kt
@@ -0,0 +1,13 @@
+package odc.game.staper
+
+import android.os.Bundle
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val config = AndroidApplicationConfiguration()
+        initialize(StaperGame(), config)
+    }
+}
